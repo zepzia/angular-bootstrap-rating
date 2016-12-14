4b999a47faf2f675438f8afc641d251a4ac2fe2b
zepzia <gwulbern@gmail.com>
2016-12-14 09:28:21 -0500

    Commit Update

--- bower_components/angular/app.js
+++ bower_components/angular/app.js
@@ -1,13 +1,16 @@
 var app = angular.module('myModule', ['ui.bootstrap']);
 
 app.controller('RatingDemoCtrl', function ($scope) {
+  $scope.titlesw= ['100%','90%','80%','70%', '60%', '50%', '40%', '30%', '20%', '10%', 'Neutral', '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'];
   $scope.rate = 7;
   $scope.max = 21;
   $scope.isReadonly = false;
 
   $scope.hoveringOver = function(value) {
+  
+    $scope.selectedTitle = $scope.titlesw[value-1];
     $scope.overStar = value;
-    $scope.percent = 100 * (value / $scope.max);
+    $scope.percent = 100 * (value / $scope.max);   
   };
 
   $scope.ratingStates = [
